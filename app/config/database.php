a06bc3e36a054e356fcf57b5cd7820994bf5eea3
Armando Ferreira Lee Eng <armando.ferreira@edu.uag.mx>
2015-07-28 22:28:48 -0500

    Added MySql/MariaDb Configuration.

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'vincula_db',
+			'username'  => 'vincula',
+			'password'  => 'Vincula$1',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
